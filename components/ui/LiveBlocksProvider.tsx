d6a94dee84571d51d3d3e229b52a5604e0726c2b
Jay D Doshi <workingjay2010@gmail.com>
2024-10-05 14:12:41 +0530

    slight changes again

--- components/ui/LiveBlocksProvider.tsx
+++ components/ui/LiveBlocksProvider.tsx
@@ -4,7 +4,7 @@ import {LiveblocksProvider} from '@liveblocks/react'
 interface LiveBlocksProviderProps {
   children: React.ReactNode;
 }
-
+console.log(process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_URL)
 function LiveBlocksProvider({ children }: LiveBlocksProviderProps) {
   if (!process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_URL) {
     throw new Error("NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_URL is not set");
